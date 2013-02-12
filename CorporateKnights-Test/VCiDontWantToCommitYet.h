b7dc5cc6af54a86dc81f52119d89ccbcf9923f2a
Darren Peacock <darrenpeacock@Darren-Peacocks-MacBook-Pro.local>
2013-02-12 16:29:04 -0500

    Part two of two stage commit

---
+++ CorporateKnights-Test/VCiDontWantToCommitYet.h
@@ -0,0 +1,13 @@
+//
+//  VCiDontWantToCommitYet.h
+//  CorporateKnights-Test
+//
+//  Created by Darren Peacock on 2013-02-12.
+//  Copyright (c) 2013 Darren Peacock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCiDontWantToCommitYet : UIViewController
+
+@end
---
+++ CorporateKnights-Test/VCiDontWantToCommitYet.m
@@ -0,0 +1,38 @@
+//
+//  VCiDontWantToCommitYet.m
+//  CorporateKnights-Test
+//
+//  Created by Darren Peacock on 2013-02-12.
+//  Copyright (c) 2013 Darren Peacock. All rights reserved.
+//
+
+#import "VCiDontWantToCommitYet.h"
+
+@interface VCiDontWantToCommitYet ()
+
+@end
+
+@implementation VCiDontWantToCommitYet
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
