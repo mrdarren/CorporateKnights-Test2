4157264383c59a57608648354e1b353be6e2cff6
Darren Peacock <darrenpeacock@Darren-Peacocks-MacBook-Pro.local>
2013-02-12 16:20:17 -0500

    Testing an Xcode commit.

--- CorporateKnights-Test/ViewController.m
+++ CorporateKnights-Test/ViewController.m
@@ -20,6 +20,8 @@
 	// Do any additional setup after loading the view, typically from a nib.
     
     // Just adding some dummy test so I can test commits in git
+    
+    // Adding more text to test an Xcode commit
 }
 
 - (void)didReceiveMemoryWarning
